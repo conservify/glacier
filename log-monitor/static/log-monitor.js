14df8b03d8685f3e8ed436f6cb4f45c89438262b
Jacob Lewallen <jlewallen@gmail.com>
2017-09-05 17:51:34 +0000

    Add cron to UI.

--- log-monitor/static/log-monitor.js
+++ log-monitor/static/log-monitor.js
@@ -148,6 +148,15 @@ class Machine extends React.Component {
             </div>
         );
     }
+
+    renderCron(cron) {
+        return (
+            <div>
+                <LastUpdatedStatus status={cron.status} time={cron.info.lastUpdatedAt} title="Cron" />
+                <LogDisplay log={cron.info.log} />
+            </div>
+        );
+    }
 }
 
 class LodgeMachine extends Machine {
@@ -158,6 +167,7 @@ class LodgeMachine extends Machine {
                 <h1>{machine.hostname}</h1>
                 {this.renderStatus(machine.health)}
                 {this.renderResilience(machine.resilience)}
+                {this.renderCron(machine.cron)}
                 {this.renderLocalBackup(machine.localBackup)}
                 {this.renderOffsiteBackup(machine.offsiteBackup)}
                 {this.renderMounts(machine.mounts)}
@@ -173,6 +183,7 @@ class GlacierMachine extends Machine {
                 <h1>{machine.hostname}</h1>
                 {this.renderStatus(machine.health)}
                 {this.renderResilience(machine.resilience)}
+                {this.renderCron(machine.cron)}
                 {this.renderLocalBackup(machine.localBackup)}
                 {this.renderOffsiteBackup(machine.offsiteBackup)}
                 {this.renderMounts(machine.mounts)}
