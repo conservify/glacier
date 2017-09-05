b8579eac0608a4c33e16bf6b16fb950699d2646a
Jacob Lewallen <jlewalle@gmail.com>
2017-09-04 20:25:18 -0700

    Log udhcpd events to syslog.

--- tce/mydata/opt/lodge/bootsync.sh
+++ tce/mydata/opt/lodge/bootsync.sh
@@ -2,4 +2,4 @@
 
 /opt/manage-iface.sh eth1 169.254.128.129 netmask 255.255.255.0 broadcast 169.254.128.255 up &
 
-/sbin/udhcpc -b -i eth0 -x hostname:lodge -p /var/run/udhcpc.eth0.pid -s /opt/lodge/udhcpc-action.sh >/dev/null 2>&1 &
+/sbin/udhcpc -S -b -i eth0 -x hostname:lodge -p /var/run/udhcpc.eth0.pid -s /opt/lodge/udhcpc-action.sh >/dev/null 2>&1 &
