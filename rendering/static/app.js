95973a512b2ade32d62edf23978dab31746b3f77
Jacob Lewallen <jlewallen@gmail.com>
2018-09-04 19:13:28 -0700

    Only render if there's something new to render.

--- rendering/static/app.js
+++ rendering/static/app.js
@@ -1,15 +1,35 @@
+let previous = null;
+
+function shouldRender(data) {
+    if (previous == null || previous.NumberOfFiles != data.NumberOfFiles || previous.Start != data.Start || previous.End != data.End) {
+        previous = data;
+        return true;
+    }
+    return false;
+}
+
 function refresh() {
-    const img = new Image();
-    img.src = "/rendering.png";
-    img.style.display = "none";
 
-    img.onload = () => {
-        console.log("Load");
-        $(img).show();
-        refresh();
-    };
+    $.getJSON('/status.json', (data) => {
+        if (shouldRender(data)) {
+            const img = new Image();
+            img.src = "/rendering.png";
+            img.style.display = "none";
 
-    $('#app').prepend(img);
+            img.onload = () => {
+                console.log("Load");
+                $(img).show();
+                refresh();
+            };
+
+            $('#app').prepend(img);
+        }
+        else {
+            setTimeout(() => {
+                refresh();
+            }, 10 * 1000);
+        }
+    });
 }
 
 refresh();
--- rendering/web_server.go
+++ rendering/web_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
 type WebServer struct {
@@ -19,12 +20,30 @@ func NewWebServer(o *options, dw *DataWatcher) (ws *WebServer, err error) {
 	return
 }
 
-func (ws *WebServer) ServeData() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		state := make(map[string]string)
+type StatusResponse struct {
+	Start         *time.Time
+	End           *time.Time
+	NumberOfFiles int
+}
 
-		b, _ := json.Marshal(state)
+func (ws *WebServer) ServeStatus() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+
+		afs := NewArchiveFileSet()
+		afs.AddFrom(ws.o.Watch)
+
+		filtered := afs.FilterLatestHour()
+
+		status := StatusResponse{
+			Start:         filtered.Start,
+			End:           filtered.End,
+			NumberOfFiles: len(filtered.Files),
+		}
+		b, _ := json.Marshal(status)
 		w.Write(b)
 	}
 }
@@ -49,6 +68,6 @@ func (ws *WebServer) ServeRendering() http.HandlerFunc {
 
 func (ws *WebServer) Register() {
 	http.Handle("/", http.FileServer(http.Dir(ws.o.Web)))
-	http.HandleFunc("/data.json", ws.ServeData())
+	http.HandleFunc("/status.json", ws.ServeStatus())
 	http.HandleFunc("/rendering.png", ws.ServeRendering())
 }
